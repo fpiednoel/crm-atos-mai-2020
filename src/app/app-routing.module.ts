733cc5d47d92ca511367170d61f1890207349766
PIEDNOEL <flavien.piednoel@atos.net>
2020-05-26 15:25:12 +0200

    routing login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,8 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules, Router } from '@angular/router';
 import { PageLoginComponent } from './login/pages/page-login/page-login.component';
 import { OrdersModule } from './orders/orders.module';
 const routes: Routes = [
-  {path: 'login', component : PageLoginComponent},
   {path: '', redirectTo : '/login', pathMatch : 'full'},
   {
     path: 'orders',
@@ -20,7 +19,18 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    enableTracing: false, // <-- debugging purposes only
+    preloadingStrategy: PreloadAllModules
+  })],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+
+  constructor(router: Router) {
+    // Use a custom replacer to display function names in the route configs
+    const replacer = (key, value) => (typeof value === 'function') ? value.name : value;
+
+    console.log('Routes: ', JSON.stringify(router.config, replacer, 2));
+  }
+}
