9d9b33531cc9287964d08284d513c852c55baaaa
PIEDNOEL <flavien.piednoel@atos.net>
2020-05-26 14:10:23 +0200

    routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-
-const routes: Routes = [];
+import { PageLoginComponent } from './login/pages/page-login/page-login.component';
+import { OrdersModule } from './orders/orders.module';
+const routes: Routes = [
+  {path: 'login', component : PageLoginComponent},
+  {path: '', redirectTo : '/login', pathMatch : 'full'},
+  {
+    path: 'orders',
+    loadChildren : () => import('./orders/orders.module').then(m => m.OrdersModule),
+  },
+  {
+    path: 'clients',
+    loadChildren : () => import('./clients/clients.module').then(m => m.ClientsModule),
+  },
+  {
+    path: '**',
+    loadChildren : () => import('./page-not-found/page-not-found.module').then(m => m.PageNotFoundModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
