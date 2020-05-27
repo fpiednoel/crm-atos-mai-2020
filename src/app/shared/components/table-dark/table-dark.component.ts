453222f2f21fa629bf0f41b3661bbb7c3a33a2c8
PIEDNOEL <flavien.piednoel@atos.net>
2020-05-27 14:59:03 +0200

    table dark

---
+++ src/app/shared/components/table-dark/table-dark.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ViewEncapsulation, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-table-dark',
+  templateUrl: './table-dark.component.html',
+  styleUrls: ['./table-dark.component.scss'],
+  encapsulation : ViewEncapsulation.None
+})
+export class TableDarkComponent implements OnInit {
+
+  @Input() headers : string[];
+
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
