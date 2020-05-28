bf8e365c1c45c2f2eeb9f03855f6b31ada0ff990
PIEDNOEL <flavien.piednoel@atos.net>
2020-05-28 16:37:15 +0200

    add form control

--- src/app/orders/component/form-add-order/form-add-order.component.ts
+++ src/app/orders/component/form-add-order/form-add-order.component.ts
@@ -1,5 +1,5 @@
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { StateOrder } from 'src/app/shared/enums/state-order.enum';
 import { Order } from 'src/app/shared/models/order';
 
@@ -9,29 +9,30 @@ import { Order } from 'src/app/shared/models/order';
   styleUrls: ['./form-add-order.component.scss']
 })
 export class FormAddOrderComponent implements OnInit {
-
-@Input() item = new Order();
-@Output() clicked: EventEmitter<Order> = new EventEmitter();
-
-public states = Object.values(StateOrder);
-public form : FormGroup;
-
-constructor(private fb : FormBuilder) { }
-
-ngOnInit(): void {
-  this.form = this.fb.group({
-    tjmHt : [this.item.tjmHt],
-    nbJours : [this.item.nbJours],
-    tva : [this.item.tva],
-    state :  [this.item.state],
-    typePresta:  [this.item.typePresta],
-    client:  [this.item.client],
-    comment:  [this.item.comment],
-    id:  [this.item.id]
-  });
+  @Input() item = new Order();
+  @Output() clicked: EventEmitter<Order> = new EventEmitter();
+  public states = Object.values(StateOrder);
+  public form: FormGroup;
+  constructor(private fb: FormBuilder) { }
+  ngOnInit(): void {
+    this.form = this.fb.group({
+      tjmHt: [this.item.tjmHt],
+      nbJours: [this.item.nbJours],
+      tva: [this.item.tva],
+      state: [this.item.state],
+      typePresta: [
+        this.item.typePresta,
+        Validators.required
+      ],
+      client: [
+        this.item.client,
+        Validators.compose([Validators.required, Validators.minLength(2)])
+      ],
+      comment: [this.item.comment],
+      id: [this.item.id],
+    });
   }
-
-  public onSubmit(){
+  public onSubmit() {
     this.clicked.emit(this.form.value);
   }
 }
--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -1,11 +1,11 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
-import { ReactiveFormsModule } from '@angular/forms';
 import { SharedModule } from '../shared/shared.module';
 import { FormAddOrderComponent } from './component/form-add-order/form-add-order.component';
 import { OrdersRoutingModule } from './orders-routing.module';
 import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
 import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 
